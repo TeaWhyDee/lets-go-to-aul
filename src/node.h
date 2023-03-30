4ae6f2e58bccd9317e01af7f3e4b23d5a95e0107
Mikhail Fedorov <fedorovm093@gmail.com>
2023-03-30 12:15:12 +0300

    chore: change codestyle

--- src/node.h
+++ src/node.h
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <vector>
-#include <string>
 #include <sstream>
+#include <string>
+#include <vector>
 
 // #include <llvm/IR/Value.h>
 
@@ -44,18 +44,20 @@ typedef std::vector<NStatement*> StatementList;
 typedef std::vector<NExpression*> ExpressionList;
 typedef std::vector<NIdentifier*> IdentifierList;
 typedef std::vector<NVariableDeclaration*> VariableList;
-typedef std::pair<NExpression *, NBlock *> conditionBlock;
+typedef std::pair<NExpression*, NBlock*> conditionBlock;
 typedef std::vector<NFunctionArgument*> functionArguments;
 
 class Visitor {
-public:
+   public:
     virtual void visitNNum(NNum* node) = 0;
     virtual void visitNNil(NNil* node) = 0;
     virtual void visitNBool(NBool* node) = 0;
     virtual void visitNString(NString* node) = 0;
     virtual void visitNIdentifier(NIdentifier* node) = 0;
-    virtual void visitNBinaryOperatorExpression(NBinaryOperatorExpression* node) = 0;
-    virtual void visitNUnaryOperatorExpression(NUnaryOperatorExpression* node) = 0;
+    virtual void visitNBinaryOperatorExpression(
+        NBinaryOperatorExpression* node) = 0;
+    virtual void visitNUnaryOperatorExpression(
+        NUnaryOperatorExpression* node) = 0;
     virtual void visitNTableField(NTableField* node) = 0;
     virtual void visitNTableConstructor(NTableConstructor* node) = 0;
     virtual void visitNFunctionDeclaration(NFunctionDeclaration* node) = 0;
@@ -75,333 +77,283 @@ public:
 };
 
 class Node {
-public:
+   public:
     virtual ~Node() {}
     virtual void visit(Visitor* v) = 0;
 };
 
-
-class NStatement : public Node {
-};
+class NStatement : public Node {};
 
 class NExpression : public NStatement {
-public:
-    virtual void visit(Visitor* v) {
-        v->visitNExpression(this);
-    }
+   public:
+    virtual void visit(Visitor* v) { v->visitNExpression(this); }
 };
 
 class NBlock : public Node {
-public:
+   public:
     StatementList statements;
     // also possible expression for return statement
-    NExpression *returnExpr;
+    NExpression* returnExpr;
 
-    NBlock() :
-        statements({}), returnExpr(nullptr) {}
+    NBlock() : statements({}), returnExpr(nullptr) {}
 
-    NBlock(StatementList statements, NExpression *returnExpr) :
-        statements(statements), returnExpr(returnExpr) { }
-    
-    NBlock(StatementList statements) :
-        statements(statements) { }
+    NBlock(StatementList statements, NExpression* returnExpr)
+        : statements(statements), returnExpr(returnExpr) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNBlock(this);
-    }
+    NBlock(StatementList statements) : statements(statements) {}
+
+    virtual void visit(Visitor* v) { v->visitNBlock(this); }
 };
 
-
 class NNum : public NExpression {
-public:
+   public:
     long double value;
-    NNum(long double value) : value(value) { }
+    NNum(long double value) : value(value) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNNum(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNNum(this); }
 };
 
 class NNil : public NExpression {
-public:
-    NNil() { }
+   public:
+    NNil() {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNNil(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNNil(this); }
 };
 
 class NBool : public NExpression {
-public:
+   public:
     bool value;
-    NBool(bool value) : value(value) { }
+    NBool(bool value) : value(value) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNBool(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNBool(this); }
 };
 
 class NString : public NExpression {
-public:
-    std::string &value;
-    NString(std::string& value) : value(value) { }
+   public:
+    std::string& value;
+    NString(std::string& value) : value(value) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNString(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNString(this); }
 };
 
 class NIdentifier : public NExpression {
-public:
+   public:
     std::string name;
-    NIdentifier(const std::string* name) : name(*name) { }
+    NIdentifier(const std::string* name) : name(*name) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNIdentifier(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNIdentifier(this); }
 };
 
 class NBinaryOperatorExpression : public NExpression {
-public:
+   public:
     NExpression* lhs;
     int op;
     NExpression* rhs;
-    NBinaryOperatorExpression(NExpression* lhs, int op, NExpression* rhs) :
-        lhs(lhs), op(op), rhs(rhs) { }
+    NBinaryOperatorExpression(NExpression* lhs, int op, NExpression* rhs)
+        : lhs(lhs), op(op), rhs(rhs) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNBinaryOperatorExpression(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNBinaryOperatorExpression(this); }
 };
 
 class NUnaryOperatorExpression : public NExpression {
-public:
+   public:
     int op;
     NExpression* rhs;
-    NUnaryOperatorExpression(int op, NExpression* rhs) :
-         op(op), rhs(rhs) { }
+    NUnaryOperatorExpression(int op, NExpression* rhs) : op(op), rhs(rhs) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNUnaryOperatorExpression(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNUnaryOperatorExpression(this); }
 };
 
 class NTableField : public Node {
-public:
+   public:
     NExpression* key;
     NExpression* value;
     NIdentifier* id;
-    NTableField(NExpression* key, NExpression* value, NIdentifier* id) :
-        key(key), value(value), id(id) { }
+    NTableField(NExpression* key, NExpression* value, NIdentifier* id)
+        : key(key), value(value), id(id) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNTableField(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNTableField(this); }
 };
 
-
 class NTableConstructor : public NExpression {
-public:
+   public:
     IdentifierList fieldlist;
-    NTableConstructor() { }
+    NTableConstructor() {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNTableConstructor(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNTableConstructor(this); }
 };
 
 class NWhileStatement : public NStatement {
-public:
+   public:
     NExpression* condition;
     NBlock* block;
-    NWhileStatement(NExpression* condition, NBlock* block) :
-        condition(condition), block(block) { }
+    NWhileStatement(NExpression* condition, NBlock* block)
+        : condition(condition), block(block) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNWhileStatement(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNWhileStatement(this); }
 };
 
 class NRepeatUntilStatement : public NStatement {
-public:
+   public:
     NExpression* condition;
     NBlock* block;
-    NRepeatUntilStatement(NExpression* condition, NBlock* block) :
-        condition(condition), block(block) { }
+    NRepeatUntilStatement(NExpression* condition, NBlock* block)
+        : condition(condition), block(block) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNRepeatStatement(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNRepeatStatement(this); }
 };
 
 class NDoStatement : public NStatement {
-public:
+   public:
     NBlock* block;
-    NDoStatement(NBlock* block) : block(block) { }
+    NDoStatement(NBlock* block) : block(block) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNDoStatement(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNDoStatement(this); }
 };
 
 class NIfStatement : public NStatement {
-public:
-    std::vector<conditionBlock *> conditionBlockList;
+   public:
+    std::vector<conditionBlock*> conditionBlockList;
     NBlock* elseBlock;
-    NIfStatement(std::vector<conditionBlock *> conditionBlockList, NBlock* elseBlock) :
-        conditionBlockList(conditionBlockList), elseBlock(elseBlock) { }
+    NIfStatement(std::vector<conditionBlock*> conditionBlockList,
+                 NBlock* elseBlock)
+        : conditionBlockList(conditionBlockList), elseBlock(elseBlock) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNIfStatement(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNIfStatement(this); }
 };
 
 class NNumericForStatement : public NStatement {
-public:
+   public:
     NIdentifier* id;
     NExpression* start;
     NExpression* end;
     NExpression* step;
     NBlock* block;
-    NNumericForStatement(NIdentifier* id, NExpression* start, NExpression* end, NExpression* step, NBlock* block) :
-        id(id), start(start), end(end), step(step), block(block) { }
+    NNumericForStatement(NIdentifier* id, NExpression* start, NExpression* end,
+                         NExpression* step, NBlock* block)
+        : id(id), start(start), end(end), step(step), block(block) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNNumericForStatement(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNNumericForStatement(this); }
 };
 
 class NGenericForStatement : public NStatement {
-public:
+   public:
     IdentifierList identifiers;
     ExpressionList expressions;
     NBlock* block;
-    NGenericForStatement(IdentifierList identifiers, ExpressionList expressions, NBlock* block) :
-        identifiers(identifiers), expressions(expressions), block(block) { }
+    NGenericForStatement(IdentifierList identifiers, ExpressionList expressions,
+                         NBlock* block)
+        : identifiers(identifiers), expressions(expressions), block(block) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNGenericForStatement(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNGenericForStatement(this); }
 };
 
+class NTypedVar : public NStatement {
+   public:
+    NIdentifier* ident;
+    NIdentifier* type;
 
-class NTypedVar: public NStatement {
-public:
-    NIdentifier *ident;
-    NIdentifier *type;
-
-    NTypedVar(NIdentifier *ident, NIdentifier *type) :
-        ident(ident), type(type) { }
+    NTypedVar(NIdentifier* ident, NIdentifier* type)
+        : ident(ident), type(type) {}
 };
 
 class NReturnStatement : public NStatement {
-public:
-    NExpression *expression;
-    NReturnStatement(NExpression *expression) :
-        expression(expression) { }
+   public:
+    NExpression* expression;
+    NReturnStatement(NExpression* expression) : expression(expression) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNReturnStatement(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNReturnStatement(this); }
 };
 
 class NDeclarationStatement : public NStatement {
-public:
-    NIdentifier *ident;
-    NIdentifier *type;
-    NExpression *expression;
-    NDeclarationStatement(NIdentifier *ident, NExpression *expression) :
-        ident(ident), type(nullptr), expression(expression) { }
+   public:
+    NIdentifier* ident;
+    NIdentifier* type;
+    NExpression* expression;
+    NDeclarationStatement(NIdentifier* ident, NExpression* expression)
+        : ident(ident), type(nullptr), expression(expression) {}
 
-    NDeclarationStatement(NIdentifier *ident, NIdentifier *type, NExpression *expression) :
-        ident(ident), type(type), expression(expression) { }
+    NDeclarationStatement(NIdentifier* ident, NIdentifier* type,
+                          NExpression* expression)
+        : ident(ident), type(type), expression(expression) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNDeclarationStatement(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNDeclarationStatement(this); }
 };
 
-class NFunctionArgument: public NStatement {
-public:
-    NIdentifier *id;
-    NIdentifier *type;
+class NFunctionArgument : public NStatement {
+   public:
+    NIdentifier* id;
+    NIdentifier* type;
 
-    NFunctionArgument(NIdentifier *id, NIdentifier *type) :
-        id(id), type(type) { }
+    NFunctionArgument(NIdentifier* id, NIdentifier* type)
+        : id(id), type(type) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNFunctionArgument(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNFunctionArgument(this); }
 };
 
 class NFunctionCall : public NExpression {
-public:
-    NExpression *expr;
+   public:
+    NExpression* expr;
 
     ExpressionList exprlist;
 
-    NFunctionCall(NExpression *expr, ExpressionList exprlist):
-        expr(expr), exprlist(exprlist) {}
+    NFunctionCall(NExpression* expr, ExpressionList exprlist)
+        : expr(expr), exprlist(exprlist) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNFunctionCall(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNFunctionCall(this); }
 };
 
 class NFunctionDeclaration : public NStatement {
-public:
-    NIdentifier *return_type;
-    NIdentifier *id;
-    std::vector<NDeclarationStatement *> *arguments;
-    NBlock *block;
+   public:
+    NIdentifier* return_type;
+    NIdentifier* id;
+    std::vector<NDeclarationStatement*>* arguments;
+    NBlock* block;
 
-    NFunctionDeclaration(NIdentifier *return_type, NIdentifier *id, 
-            std::vector<NDeclarationStatement *> *arguments, NBlock *block) :
-        return_type(return_type), id(id), arguments(arguments), block(block) { }
+    NFunctionDeclaration(NIdentifier* return_type, NIdentifier* id,
+                         std::vector<NDeclarationStatement*>* arguments,
+                         NBlock* block)
+        : return_type(return_type),
+          id(id),
+          arguments(arguments),
+          block(block) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNFunctionDeclaration(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNFunctionDeclaration(this); }
 };
 
 class StructBody {
-public:
-    std::vector<NDeclarationStatement *> fields;
-    std::vector<NFunctionDeclaration *> methods;
+   public:
+    std::vector<NDeclarationStatement*> fields;
+    std::vector<NFunctionDeclaration*> methods;
 
-    StructBody() :
-        fields(std::vector<NDeclarationStatement *>()), methods(std::vector<NFunctionDeclaration *>()) { };
+    StructBody()
+        : fields(std::vector<NDeclarationStatement*>()),
+          methods(std::vector<NFunctionDeclaration*>()){};
 };
 
 class NStructDeclaration : public NStatement {
-public:
-    NIdentifier *id;
-    std::vector<NDeclarationStatement *> fields;
-    std::vector<NFunctionDeclaration *> methods;
+   public:
+    NIdentifier* id;
+    std::vector<NDeclarationStatement*> fields;
+    std::vector<NFunctionDeclaration*> methods;
 
-    NStructDeclaration(NIdentifier *id, StructBody *body) :
-        id(id), fields(body->fields), methods(body->methods) { }
+    NStructDeclaration(NIdentifier* id, StructBody* body)
+        : id(id), fields(body->fields), methods(body->methods) {}
 
-    virtual void visit(Visitor* v) {
-        v->visitNStructDeclaration(this);
-    }
+    virtual void visit(Visitor* v) { v->visitNStructDeclaration(this); }
 };
 
 class PrettyPrintVisitor : public Visitor {
-public:
+   public:
     int tabs;
-    PrettyPrintVisitor() : tabs(0) { }
-    const std::string indent() {
-        return std::string(4 * this->tabs, ' ');
-    }
+    PrettyPrintVisitor() : tabs(0) {}
+    const std::string indent() { return std::string(4 * this->tabs, ' '); }
 
     virtual void visitNNum(NNum* node) {
         std::cout << "NNum(value=" << node->value << ")";
     }
 
-    virtual void visitNNil(NNil* node) {
-        std::cout << "NNil()";
-    }
+    virtual void visitNNil(NNil* node) { std::cout << "NNil()"; }
 
     virtual void visitNBool(NBool* node) {
         std::cout << "NBool(value=" << node->value << ")";
@@ -415,7 +367,8 @@ public:
         std::cout << "NIdentifier(name=" << node->name << ")";
     }
 
-    virtual void visitNBinaryOperatorExpression(NBinaryOperatorExpression* node) {
+    virtual void visitNBinaryOperatorExpression(
+        NBinaryOperatorExpression* node) {
         std::cout << "NBinaryOperatorExpression";
         std::cout << "(lhs=";
         node->lhs->visit(this);
@@ -442,13 +395,12 @@ public:
 
     virtual void visitNTableConstructor(NTableConstructor* node) {
         std::cout << "NTableConstructor(fields=[";
-         for (auto field: node->fieldlist) {
+        for (auto field : node->fieldlist) {
             field->visit(this);
             std::cout << ", ";
         }
         std::cout << "])";
     }
-
 
     virtual void visitNFunctionDeclaration(NFunctionDeclaration* node) {
         std::cout << "NFunctionDeclaration(id=";
@@ -461,10 +413,9 @@ public:
         std::cout << ", block=[\n\t";
         node->block->visit(this);
         std::cout << "], arguments=[";
-        for (auto arg: *node->arguments) {
+        for (auto arg : *node->arguments) {
             arg->visit(this);
-            if (arg != node->arguments->back())
-                std::cout << ", ";
+            if (arg != node->arguments->back()) std::cout << ", ";
         }
         std::cout << "])";
     }
@@ -473,7 +424,7 @@ public:
         std::cout << "NFunctionCall(expr=";
         node->expr->visit(this);
         std::cout << ", expr_list=[";
-        for(auto expr: node->exprlist) {
+        for (auto expr : node->exprlist) {
             expr->visit(this);
             std::cout << ", ";
         }
@@ -512,7 +463,7 @@ public:
 
     virtual void visitNIfStatement(NIfStatement* node) {
         std::cout << "NIfStatement(conditions=[";
-        for (auto clause: node->conditionBlockList) {
+        for (auto clause : node->conditionBlockList) {
             std::cout << "condition=";
             clause->first->visit(this);
             std::cout << "block=\n\t";
@@ -539,19 +490,19 @@ public:
         node->step->visit(this);
         std::cout << ", block=\n\t";
         node->block->visit(this);
-        std::cout<< ")";
+        std::cout << ")";
     }
 
     virtual void visitNGenericForStatement(NGenericForStatement* node) {
         std::cout << "NGenericForStatement(identifiers=[";
-        for (auto ident: node->identifiers) {
+        for (auto ident : node->identifiers) {
             ident->visit(this);
             if (ident != node->identifiers.back()) {
                 std::cout << ", ";
             }
         }
         std::cout << "], expressions=[";
-        for (auto expr: node->expressions) {
+        for (auto expr : node->expressions) {
             expr->visit(this);
             if (expr != node->expressions.back()) {
                 std::cout << ", ";
@@ -583,10 +534,10 @@ public:
 
     virtual void visitNBlock(NBlock* node) {
         std::cout << "NBlock(\n  statements=[" << std::endl;
-        for (auto stmt: node->statements) {
+        for (auto stmt : node->statements) {
             std::cout << "    ";
             stmt->visit(this);
-            std::cout<<std::endl;
+            std::cout << std::endl;
         }
 
         std::cout << "  ]";
@@ -605,14 +556,14 @@ public:
         std::cout << "NStructDeclaration(id=";
         node->id->visit(this);
         std::cout << ", fields=[";
-        for (auto field: node->fields) {
+        for (auto field : node->fields) {
             field->visit(this);
             if (field != node->fields.back()) {
                 std::cout << ", ";
             }
         }
         std::cout << "], methods=[";
-        for (auto field: node->methods) {
+        for (auto field : node->methods) {
             field->visit(this);
             if (field != node->methods.back()) {
                 std::cout << ", ";
