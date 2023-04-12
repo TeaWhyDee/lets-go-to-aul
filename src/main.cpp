acf2198b0f48c4cafb569813b41bde18bc164b6f
Mikhail Fedorov <fedorovm093@gmail.com>
2023-04-12 09:44:13 +0300

    feat: add space between visotor output

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,21 @@
 #include "node.h"
 
 extern NBlock* programBlock;
-extern SymbolTableStorage *symtab_storage;
+extern SymbolTableStorage* symtab_storage;
 extern int yyparse();
 
 int main(int argc, char** argv) {
     yyparse();
     auto pretty_print_visitor = new PrettyPrintVisitor();
-    Visitor *visitors[] = {
+    Visitor* visitors[] = {
         new TypeChecker(pretty_print_visitor),
         pretty_print_visitor,
         new SymbolTableFillerVisitor(),
         new DeclaredBeforeUseCheckerVisitor(),
     };
-    try{
-        for (auto visitor: visitors) {
-            std::cout << "visit " << visitor->name << std::endl;
+    try {
+        for (auto visitor : visitors) {
+            std::cout << "\n\n------------------" << visitor->name << "------------------" << std::endl;
             programBlock->visit(visitor);
             std::cout << std::endl;
             visitor->cleanup();
