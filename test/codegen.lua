f109a963544761287d32294b81ae752ab06b041f
Mikhail Fedorov <fedorovm093@gmail.com>
2023-05-08 17:19:41 +0300

    feat: change codegen test

--- test/codegen.lua
+++ test/codegen.lua
@@ -1,41 +1,69 @@
-a = 3
-b = 5
-c = 10
-e = "1323"
-
-d = (a - b) + c
-
-printf("hello the value is %.3f, str is %s\n", d, e)
+-- printf("Goodbye, Aul!\n")
 -- a = 3
--- do
---     a = 2
--- end
-
+-- b = 5
+-- c = 10
+-- e = "1323"
 --
--- while a == 3 do
---   a = 4
--- end
+-- d = (a - b) + c
 --
+-- printf("Values of a,b,c,d: %.1f, %.1f, %.1f, %.1f, '%s'\n", d, e)
 
-printf("value of a is %.3f\n", a)
--- for i = 1, 2, 1 do
---   a = 3
+
+-- --Check while block
+-- printf("\nWHILE\n")
+-- a = 10
+-- printf("A before = %.3f\n", a)
+-- while a > 0 do
+--   a = a - 1
+--   printf("A = %.3f\n", a)
 -- end
+-- printf("A after = %.3f\n", a)
 
 
-if 1 == 2 then
-  e=1
-elseif 1 == 2 then
-  f=2
-elseif 2 == 2 then
-  a = 4
-end
-b = 2
-
-printf("value of a is %.3f\n", a)
+-- --Check do end block
+-- printf("\nDO END\n")
 -- a = 1
--- if 1 == 2 then
---   e=1
+-- printf("A before = %.3f\n", a)
+-- do
+--   b: num = 14
+--   a = a * b
+--   printf("A = %.3f\n", a)
+--   printf("B = %.3f\n", b)
+-- end
+-- printf("A after = %.3f\n", a)
+
+
+-- --Check repeat until block
+-- printf("\nREPEAT UNTIL\n")
+-- a = 1
+-- repeat
+--   printf("A = %.3f\n", a)
+--   a = a * 2
+-- until a ~= 8
+-- printf("A after = %.1f\n", a)
+
+
+-- --Next part is not working for now!
+
+-- --Check if block (for now not working)
+-- printf("\nIF\n")
+-- a = 14
+-- printf("A is %.3f\n", a)
+-- if a < 10 then
+--   a = 12
+--   printf("A = %.3f\n", a)
+-- elseif a > 20 then
+--   a = 22
+--   printf("A = %.3f\n", a)
 -- else
---   g=3
+--   a = 32
+--   printf("A = %.3f\n", a)
+-- end
+-- printf("A after = %.3f\n", a)
+
+
+-- --Check numeric for (for now not working)
+-- printf("\nNUMERIC FOR\n")
+-- for i = 1, 10, 1 do
+--   printf("I = %.3f\n", i)
 -- end
