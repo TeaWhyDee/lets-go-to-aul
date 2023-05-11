9e2c8e1b7c731c923906e9e666e1151b6ce8bcdb
teawd <52975098+TeaWhyDee@users.noreply.github.com>
2023-05-11 18:34:17 +0200

    Add: unary op codegen (#95)
    * fix: typeChecker
    
    * add: unop codegen

--- src/node.h
+++ src/node.h
@@ -1483,13 +1483,6 @@ class TypeChecker : public SymtabVisitor {
         std::cout << "UnaryOperatorExpression(";
         node->visit(this->prettyPrinter);
         node->rhs->visit(this);
-        if (node->rhs->type != nullptr) {
-            node->type = new NBoolType();
-        } else {
-            std::cout << "TypeError: type mismatch, rhs type is not defined)" << std::endl;
-            throw SemanticError("Unary operator type mismatch", node->position);
-            return;
-        }
 
         // possible operators for num:
         auto num_allowed_ops = std::vector<UnOpType>{UnOpType::MINUS};
@@ -1503,12 +1496,18 @@ class TypeChecker : public SymtabVisitor {
                 throw SemanticError("Unary operator: operator not allowed for type num", node->position);
                 return;
             }
+            else {
+                node->type = new NNumType();
+            }
         } else if (dynamic_cast<NBoolType*>(node->rhs->type) != nullptr) {
             // check if the operator is in the list of possible operators
             if (std::find(bool_allowed_ops.begin(), bool_allowed_ops.end(), node->op) == bool_allowed_ops.end()) {
                 std::cout << "TypeError: operator " << node->op << " is not allowed for type bool" << std::endl;
                 throw SemanticError("Unary operator: operator not allowed for type bool", node->position);
                 return;
+            }
+            else {
+                node->type = new NBoolType();
             }
         } else {
             std::cout << "TypeError: operator " << node->op << " is not allowed for type " << node->rhs->type << std::endl;
@@ -2633,6 +2632,14 @@ class CodeGenVisitor : public SymtabVisitor {
 
     virtual void visitNUnaryOperatorExpression(NUnaryOperatorExpression* node) {
         node->rhs->visit(this);
+        switch (node->op) {
+            case UnOpType::MINUS:
+                node->llvm_value = this->builder->CreateFNeg(node->rhs->llvm_value);
+                break;
+            case UnOpType::NOT:
+                node->llvm_value = this->builder->CreateNot(node->rhs->llvm_value);
+                break;
+        }
         // Value *OperandV = node->rhs->llvm_value;
         // Function *func = Function::Create(std::string("unary") + std::to_string(node->op));
         // node->llvm_value = this->builder->CreateCall(func, OperandV, "unop");
