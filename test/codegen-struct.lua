7af34f04ba9f5a304991d503e7592c3b25b1281f
Ilya Siluyanov <isiluyanov8@gmail.com>
2023-05-12 09:44:58 +0300

    Back to working example

--- test/codegen-struct.lua
+++ test/codegen-struct.lua
@@ -14,4 +14,19 @@ end
 p1 = Point(5, 5)
 p2 = Point(10, 10)
 
-printf("%f\n", p2.dist(p1))
+
+function max(a: num, b: num) -> num
+    if a > b then
+        return a
+    else
+        return b
+    end
+end
+
+
+function manhattan(p1: Point, p2: Point) -> num
+    return max(p1.x - p2.x, p2.x - p1.x) + max(p1.y - p2.y, p2.y - p1.y)
+end
+
+printf("Euclidian: %f\n", p1.dist(p2))
+printf("Manhattan: %f\n", manhattan(p1, p2))
